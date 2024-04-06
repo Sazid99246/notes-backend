ab72e05baba68eade61171d728cd3ce676ca6430
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-04-06 23:20:47 +0600

    refactored and reorganized project structure

--- index.js
+++ index.js
@@ -1,58 +1,6 @@
-const Note = require('./models/note')
-const express = require('express')
-const cors = require('cors')
+const app = require('./app')
+const config = require('./utils/config')
 
-const app = express()
-
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: Date()
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${config.PORT}`)
 })
--- models/note.js
+++ models/note.js
@@ -1,27 +1,19 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
-
-const url = process.env.MONGODB_URL
-
-mongoose.set('strictQuery', false)
-
-mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
-  date: Date,
   important: Boolean
 })
 
 noteSchema.set('toJSON', {
-  transform: (document, returnObject) => {
-    returnObject.id = returnObject._id.toString()
-    delete returnObject._id
-    delete returnObject.__v
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
 })
 
