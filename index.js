ae3f710cd983ed0080e36a4e1220ab730cd95b00
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-04-07 19:56:27 +0600

    PORT added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 const config = require('./utils/config')
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
